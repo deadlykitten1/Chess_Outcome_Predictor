a621d46ec842beb0f8c28602acbc546acd0ddaa2
deadlykitten1 <agcwill1@gmail.com>
2022-04-18 12:12:36 -0400

    fixed df to not contain magnuses

--- scraping_helper_functions.py
+++ scraping_helper_functions.py
@@ -1,8 +1,10 @@
 from cmath import exp
+from hashlib import new
 from shutil import which
 from bs4 import BeautifulSoup
 import numpy as np
 import requests
+import pandas as pd
 
 def cook_chess_soup(URL):
     """
@@ -82,3 +84,35 @@ def get_chess_data(chunk):
         opening = np.nan
 
     return white_player, white_player_rating, black_player, black_player_rating, result, game_length, year, opening_name, opening
+
+
+def player_df_cleanup(row, name):
+    """
+    Helper function to transform our with our player's name in each column into just a df of their opponents' info
+    Returns a row as a list with relevant information
+    """
+    new_row = []
+
+    if row[0] == name: # player plays white
+        new_row.append(row[1]) # opp
+        new_row.append(1) # opp color (1 is black)
+        new_row.append(row[3]) # opp rating
+        new_row.append(row[2]) # player rating
+    else: # plyaer plays black
+        new_row.append(row[0]) # opp
+        new_row.append(0) # opp color (1 is black)
+        new_row.append(row[2]) # opp rating
+        new_row.append(row[3]) # player rating
+
+    # if wp == name: # player plays white
+    #     new_row.append(bp) # opp
+    #     new_row.append(1) # opp color (1 is black)
+    #     new_row.append(bpr) # opp rating
+    #     new_row.append(wpr) # player rating
+    # else: # plyaer plays black
+    #     new_row.append(wp) # opp
+    #     new_row.append(0) # opp color (0 is white)
+    #     new_row.append(wpr) # opp rating
+    #     new_row.append(bpr) # player rating
+    
+    return new_row
